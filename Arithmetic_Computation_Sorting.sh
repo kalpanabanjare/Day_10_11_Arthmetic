796684e5787ec95d3f5fbed6590f6196136799a6
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-02-23 22:48:37 +0530

    Added: computed value in dictionary

--- Arithmetic_Computation_Sorting.sh
+++ Arithmetic_Computation_Sorting.sh
@@ -17,3 +17,17 @@ echo "c+a/b $result_3"
 
 result_4=$(( (($a % $b) + $c) | bc ))
 echo "a%b+c $result_4"
+
+result[111]=$((result_1))
+echo "result[111]="${result[111]}
+
+result[222]=$((result_2))
+echo "result[222]="${result[222]}
+
+result[333]=$((result_3))
+echo "result[333]="${result[333]}
+
+result[444]=$((result_4))
+echo "result[444]="${result[444]}
+
+
