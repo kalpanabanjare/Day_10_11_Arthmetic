7f63ce188a23620efa4e33c9337acd3ff5820ef4
kalpanabanjare <kalpanabanjare99@gmail.com>
2022-02-24 16:25:10 +0530

    Added: Ascending order

--- Arithmetic_Computation_Sorting.sh
+++ Arithmetic_Computation_Sorting.sh
@@ -53,3 +53,19 @@ do
 done
 
 echo "Descending Order : "${arr[*]}
+
+for ((i = 0; i<4; i++))
+   do
+      for((j = 0; j<4-i-1; j++))
+      	do
+          if [ $((${arr[j]})) -gt $((${arr[$((j+1))]})) ]
+          then
+      	  #SWAPPING
+                   temp=${arr[j]}
+                   arr[$j]=${arr[$((j+1))]}
+                   arr[$((j+1))]=$temp
+          fi
+      done
+done
+
+echo "Ascending Order "${arr[*]}
